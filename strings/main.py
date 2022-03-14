9d30fda96903765bb8898d6b389671f2df22de76
Maarten Van Poucke <91698117+Maarten-MVP@users.noreply.github.com>
2022-03-14 18:52:25 +0100

    Add files via upload

--- strings/main.py
+++ strings/main.py
@@ -10,17 +10,17 @@ scoring_player2 = 'Marco van Basten'
 goal_0 = 32
 goal_1 = 54
 
-scorers = f'{scoring_player1} {goal_0}, {scoring_player2} {goal_1}'
+scorers = scoring_player1 + " " + str(goal_0) +", " + scoring_player2 + " " + str(goal_1)
 
-report = f'{scoring_player1} scored in the {goal_0}nd minute \n{scoring_player2} scored in the {goal_1}th minute'
+report = f'{scoring_player1} scored in the {goal_0}nd minute\n{scoring_player2} scored in the {goal_1}th minute'
 
 player = 'Ronald Koeman'
 
-first_name = player[0:player.find(' ')]
+first_name = player[:player.find(' ')]
 
 last_name_len = len(player[player.find(' ')+1:])
 
-name_short = player[0:1] + '.' + player[player.find(' '):]
+name_short = player[0] + '.' + player[player.find(' '):]
 
 chant = f'{first_name}! ' * (len(first_name)-1) + f'{first_name}!'
 
